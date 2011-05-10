eb9a4e73d107009ba5debb2c0dd3fe45728ee178
Arne Eilermann <eilermann@lavabit.com>
2011-05-10 22:39:15 +0200

    Switch to pretty urls without this pagename/index.html stuff.

---
+++ _plugins/ext.rb
@@ -0,0 +1,19 @@
+module Jekyll
+
+  class Post
+
+    alias_method :_original_url, :url
+
+    def url
+      _original_url.sub(/\/\z/, '')
+    end
+
+    alias_method :_original_destination, :destination
+
+    def destination(dest)
+      File.join(dest, CGI.unescape("#{url}.html"))
+    end
+
+  end
+
+end
